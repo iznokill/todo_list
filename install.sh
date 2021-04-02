cb942714a5b4d1f83734a946cabf1c77dc86dafd
Izno (Youssef Kadaoui Abbassi) <64014493+iznokill@users.noreply.github.com>
2021-04-02 19:19:02 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ mv TODO/ ~/
 
 echo -e "##############todo_list############\n" >> ~/.bashrc
 echo -e "alias todo=\"sh ~/TODO/.config/todo.sh\"\n" >> ~/.bashrc
-echo -e "alias whattodo=\"sh ~/TODO/.config/whattodo.sh\"\n" >> ~/.bashrc
+echo -e "alias whattodo=\"sh ~/TODO/.config/wtodo.sh\"\n" >> ~/.bashrc
 echo -e "alias nottodo=\"sh ~/TODO/.config/nottodo.sh\"\n" >> ~/.bashrc
 echo -e "alias cleando=\"sh ~/TODO/.config/cleando.sh\"\n" >> ~/.bashrc
 echo -e "alias ccleando=\"sh ~/TODO/.config/ccleando.sh\"\n" >> ~/.bashrc
